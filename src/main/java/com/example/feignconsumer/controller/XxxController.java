d2b18b855c2ec71c4f7be2eee0bc44073d68dc4d
leiming5 <leiming5@lenovo.com>
2022-04-11 17:17:42 +0800

    还原 到代码初始状态，应用能够启动

--- src/main/java/com/example/feignconsumer/controller/XxxController.java
+++ src/main/java/com/example/feignconsumer/controller/XxxController.java
@@ -1,13 +1,8 @@
 package com.example.feignconsumer.controller;
 
-import com.lenovo.npi.auth.center.service.impl.AcUserServiceImpl;
-import com.lenovo.npi.portal.module.masterdata.controller.CommonController;
-import com.lenovo.platform.common.service.BaseService;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 /**
  * @description: XxxController
@@ -17,14 +12,6 @@ import javax.annotation.Resource;
 @RestController
 public class XxxController {
 
-    @Resource
-    BaseService baseService;
-    @Resource
-    private ValidateClient validateClient;
-    @Resource
-    private CommonController commonController;
-    @Resource
-    private AcUserServiceImpl userService;
 
     @GetMapping("feign/test")
     public String feignTest(String xxxx) {
